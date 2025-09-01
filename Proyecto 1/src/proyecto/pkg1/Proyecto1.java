15cdae455f9e9458409b317bd974041fbe9e2141
Juanjo13333 <3184689560718@ingenieria.usac.edu.gt>
2025-08-31 22:38:26 -0600

    Proyecto

---
+++ Proyecto 1/src/proyecto/pkg1/Proyecto1.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto.pkg1;
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
