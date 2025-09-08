52c7ed6fea6940258a290eaebefaa3bb8a742ec0
Juanjo13333 <3184689560718@ingenieria.usac.edu.gt>
2025-09-07 22:38:19 -0600

    agregado

--- Proyecto 1/src/proyecto/pkg1/Proyecto1.java
+++ Proyecto 1/src/proyecto/pkg1/Proyecto1.java
@@ -6,14 +6,17 @@ import java.util.Scanner;
 public class Proyecto1 {
     
     static int elementosMax = 100;
-    static String codigos [] = new String [elementosMax];
+    static String codigo [] = new String [elementosMax];
     static String nombres [] = new String [elementosMax];
+    static String categorias [] = new String[elementosMax]; 
+    static float precio [] = new float [elementosMax];
+    static int stocks [] = new int [elementosMax];
+    static int totalProductos = 0;
     
-    
+    static Scanner entrada = new Scanner(System.in);
     
     public static void main(String[] args) {
-       Scanner entrada = new Scanner(System.in);
-       
+        
        int opcion =0;  
 
                
@@ -51,7 +54,7 @@ public class Proyecto1 {
                         verDatos();
                         break;
                     case 7:
-                        Bitacora();
+                        bitacora();
                         break;
                     case 8:
                         salir();
@@ -62,8 +65,71 @@ public class Proyecto1 {
             }catch(NumberFormatException e){
                 System.out.println("Error: Debe ingresar un numero valido");
             }
-       }while(opcion != 8);
-
-       
+       }while(opcion != 8); 
     }
+    
+    
+    public static void agregarProducto(){ 
+       //El codigo verifica que no se haya excedido el numero de articulos
+       //si el codigo no ha excedido el numero de produtos, no recorrera este bloque
+       
+       if (totalProductos >= elementosMax){
+           System.out.println("Llego al limite de compra, por favor haga una nueva compra");
+           return;
+       }
+       
+       System.out.println("Agregue un producto a su compra :)");
+       
+       //Pedir el codigo de un articulo 
+       boolean codigoUnico = false;
+       int contador = 0;
+       double precio;
+       String nuevoCodigo, categoria;
+       
+       do{
+       System.out.println("Ingrese el codigo del articulo: ");
+       nuevoCodigo = entrada.nextLine().trim();
+       if (nuevoCodigo.isEmpty()){
+           System.out.println("Error: el codigo esta vacio, por favor digite un codigo valido");
+       }
+       
+       //Validar que el codigo sea unico
+       codigoUnico = true;
+       for(int i = 0; i <totalProductos; i++){
+           if(codigo[i] != null && codigo[i].equalsIgnoreCase(nuevoCodigo))
+               System.out.println("Error: este codigo ya existe");
+           codigoUnico = false;
+           break;
+       } 
+       }while(!codigoUnico);
+       
+       //Agregar a la compra el nombre de un articulo
+        System.out.println("Que producto desea comprar?: ");
+        String nombre = entrada.nextLine().trim();
+        if (nombre.isEmpty()){
+            System.out.println("Error: debe ingresar el nombre de algun producto");
+            return;
+        }
+        
+        
+        System.out.println("Que desea comprar: \nPantalones:\n, \nPlayeras:\n, \nCalcetines:\n, \nRopa interior:\n, \nSueteres:\n, \nGorras:\n ");
+        categoria = entrada.nextLine().trim();
+        
+        for(int i=0; i<totalProductos; i++){
+            if (categorias[i] != null && categorias [i].equalsIgnoreCase(categoria)){
+                contador++;
+            }
+        }
+        if (contador >= 5){
+            System.out.println("Solo puedes escoger 5 productos de cada categoria"+categoria);
+            return;
+        }
+       
+
+ 
+        
+    }
+    
+    
+    
 }   
